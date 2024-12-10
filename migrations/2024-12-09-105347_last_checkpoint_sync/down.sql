0b00e2a789fc2755586b46c1e1b0522871e6ed05
Sergiu Popescu <sergiu.popescu@iota.org>
2024-12-10 17:45:49 +0100

    fix: remove unnecessary index

--- migrations/2024-12-09-105347_last_checkpoint_sync/down.sql
+++ migrations/2024-12-09-105347_last_checkpoint_sync/down.sql
@@ -1,4 +1,2 @@
 -- This file should undo anything in `up.sql`
-DROP INDEX checkpoint_sequence_number;
-
 DROP TABLE last_checkpoint_sync;
--- migrations/2024-12-09-105347_last_checkpoint_sync/up.sql
+++ migrations/2024-12-09-105347_last_checkpoint_sync/up.sql
@@ -3,5 +3,3 @@ CREATE TABLE IF NOT EXISTS last_checkpoint_sync (
     task_id TEXT NOT NULL PRIMARY KEY,
     sequence_number INTEGER NOT NULL
 );
-
-CREATE INDEX IF NOT EXISTS checkpoint_sequence_number ON last_checkpoint_sync (task_id, sequence_number);
